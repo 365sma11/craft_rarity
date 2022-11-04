7f09cf3eb06eb0821fdcb861bf256e5ab7eb7e60
sma11 <36231823+365sma11@users.noreply.github.com>
2022-11-04 17:15:19 -0600

    vfinal

--- rarity_craft.py
+++ rarity_craft.py
@@ -48,11 +48,27 @@ def plot(dataframe):
 
     st.plotly_chart(plot)
 
-
+def range(range):
+    if range=='10':
+        col1, col2 = st.columns(2) 
+        with col1:
+            st.write("Rank Range")
+            df_range= pd.DataFrame(
+                {'Model':["M","E","O","W","Z"],
+                'Top':["62","58","18","5","1"]})
+            st.write(df_range)
+        with col2:
+            st.write("Model Count")
+            df_range= pd.DataFrame(
+            {'Model':["M","E","O","W","Z"],
+            'Top':["62","58","18","5","1"]})
+            st.write(df_range)
+    
 
 
 set_background('craft.png')
 st.title ('Crafts Rarity - Model Multiplier Compare')
+st.text ('Click on the Different Model Types on the Right to turn on/off Models for comparison/isolation.')
 st.sidebar.title("MODEL MULTIPLIER")
 options=st.sidebar.radio('MODEL MULTIPLIER',options=['NO MULTIPLIER','10X','25X','100X'])
 
@@ -65,15 +81,18 @@ if options == 'NO MULTIPLIER':
 elif options == '10X':
     df=pd.read_csv("crafts_rank10x.csv")
     plot(df)
+    st.sidebar.image("10x range.png")
        
 
 elif options == '25X':
     df=pd.read_csv("crafts_rank25x.csv")
     plot(df)
+    st.sidebar.image("25x range.png")
 
 elif options == '100X':
     df=pd.read_csv("crafts_rank100x.csv")
     plot(df)
+    st.sidebar.image("100x range.png")
 
 
 
